353daa262fbf1cce36f0daa1a4a2adb16cf2fb6f
Alex <alexkravchenko27@gmail.com>
2022-02-01 12:42:47 +0200

    disable input done button and add comments

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ button.addEventListener('click', () => {
   const newDiv = document.createElement('div');
   newDiv.classList.add('newElems');
 
+  // add new task 
   const newTask = document.createElement('div');
   newTask.classList.add('newTask')
   newTask.innerHTML = `${input.value}`;
@@ -14,11 +15,12 @@ button.addEventListener('click', () => {
     return
   }
 
-
+  // add new cancel button
   const cancelTask = document.createElement('div');
   cancelTask.classList.add('cancelTask')
   cancelTask.innerHTML = 'X';
 
+  // add new important button
   const newImportant = document.createElement('div');
   newImportant.classList.add('newImportant')
   newImportant.innerHTML = 'V';
@@ -28,6 +30,7 @@ button.addEventListener('click', () => {
   newDiv.appendChild(cancelTask)
   cancelTask.before(newImportant)
 
+  //delete some task
   cancelTask.addEventListener('click', (elem) => {
     const item = elem.target;
     if (item.classList[0] === 'cancelTask') {
@@ -36,48 +39,57 @@ button.addEventListener('click', () => {
     }
   })
 
+  //add green color if important
   newImportant.addEventListener('click', (elem) => {
     const item = elem.target;
     const toDo = item.parentElement;
     toDo.classList.toggle('toggleColor')
   })
 
+  //clear input
   input.value = '';
 })
 
 const sel = document.querySelector('.select');
-sel.addEventListener('click', () => {
-  let newElems = document.querySelectorAll('.newElems');
+sel.addEventListener('change', () => {
+  const newElems = document.querySelectorAll('.newElems');
 
+  // sort "in progress" button
   if (sel.value == 'inProgress') {
-    for (let i = 0; i < newElems.length; i++) {
+    input.disabled = false;
 
+    for (let i = 0; i < newElems.length; i++) {
       if (newElems[i].classList.contains('toggleColor')) {
         newElems[i].classList.add('none');
 
       } else if (newElems[i].classList.contains('none')) {
         newElems[i].classList.remove('none');
       }
-
     }
 
+    // sort "all" button
   } else if (sel.value == 'all') {
+    input.disabled = false;
     for (let i = 0; i < newElems.length; i++) {
       newElems[i].classList.remove('none');
     }
 
+    // sort "done" button
   } else if (sel.value == 'done') {
-    for (let i = 0; i < newElems.length; i++) {
+    input.disabled = true;
 
+    for (let i = 0; i < newElems.length; i++) {
       if (newElems[i].classList.contains('toggleColor') == false) {
         newElems[i].classList.add('none');
 
       } else if (newElems[i].classList.contains('none')) {
         newElems[i].classList.remove('none');
       }
-
     }
+
   }
 })
+
+
 
 
